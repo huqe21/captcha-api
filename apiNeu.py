93092632c639ea5f4dfc8ff6f32e231b718af5fd
Huqe21 <huqe21@icloud.com>
2024-04-09 19:23:08 +0200

    added db

--- apiNeu.py
+++ apiNeu.py
@@ -1,10 +1,11 @@
 from flask import Flask, request
 from flask_cors import CORS
-from config import ModelConfigs
-from imageToWordModel import ImageToWordModel
+#from config import ModelConfigs
+#from imageToWordModel import ImageToWordModel
 import cv2
 import os
 from apiConfig import Config
+from pymongo import MongoClient
 
 
 app = Flask(__name__)
@@ -14,35 +15,37 @@ UPLOAD_FOLDER = 'uploads'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-configs = ModelConfigs.load("Models/202403051356/configs.yaml")
+#configs = ModelConfigs.load("Models/202403051356/configs.yaml")
 
-model = ImageToWordModel(model_path=configs.model_path, char_list=configs.vocab)
+#model = ImageToWordModel(model_path=configs.model_path, char_list=configs.vocab)
 
 client = MongoClient(Config.MONGO_URI)
 db = client['valid8users']
 users_collection = db ['users']
 
 
-@app.route('/captcha-solver', methods=['POST'])
+@app.route('/captcha-solver', methods=['GET'])
 def solve_captcha():
-    json = ""
-    if 'file' not in request.files:
-        return 'No file part in the request', 400
+    
+   
+    json =  users_collection.find()
+#     if 'file' not in request.files:
+#         return 'No file part in the request', 400
 
-    file = request.files['file']
-    if file.filename == '':
-        return 'No selected file', 400
+#     file = request.files['file']
+#     if file.filename == '':
+#         return 'No selected file', 400
 
-    if file:
-        filename = file.filename
-        file.save(os.path.join(UPLOAD_FOLDER, filename))
+#     if file:
+#         filename = file.filename
+#         file.save(os.path.join(UPLOAD_FOLDER, filename))
 
-        image = cv2.imread(os.path.join(UPLOAD_FOLDER, filename))
-        prediction = model.predict(image)
-        json = {'captcha': str(prediction)}
+#         image = cv2.imread(os.path.join(UPLOAD_FOLDER, filename))
+# #        prediction = model.predict(image)
+#  #       json = {'captcha': str(prediction)}
 
-    if os.path.exists(os.path.join(UPLOAD_FOLDER, file.filename)):
-        os.remove(os.path.join(UPLOAD_FOLDER, file.filename))
+#     if os.path.exists(os.path.join(UPLOAD_FOLDER, file.filename)):
+#         os.remove(os.path.join(UPLOAD_FOLDER, file.filename))
 
     return json, 200
 
--- decodeJwt.py
+++ decodeJwt.py
@@ -2,7 +2,7 @@ import requests
 
 
 def validate_and_decode_google_token(token):
-    userinfo_url = 'https://www.googleapis.com/oauth2/v2/userinfo'
+    userinfo_url = 'https://www.googleapis.com/oauth2/v3/userinfo'
     
     # Header für die Anfrage, einschließlich des Access Tokens
     headers = {'Authorization': f'Bearer {token}'}
@@ -21,7 +21,7 @@ def validate_and_decode_google_token(token):
         return None
 
 # Beispiel Token
-token = 'ya29.a0Ad52N39uu2FQzpsbUxUIXX7gYz8YJnndxmpqj44aWsKVfXsTC-1aQdURlrFoRNYGPGln5wen4RE00wTxyWIY2Y7UPtXoahwXdhB5paAhvmnLnRxY8jvHHf5bb3gXWl9T8fIo0vQNeMkBYQy3GzHbdAF1CQ0TpFKXbZwaCgYKAaoSARESFQHGX2MiZXUiphNOAYoc5NV_IBdf_A0170'
+token = 'ya29.a0Ad52N3-VWvkIBbIOJrz24YRn-u5O6oDJG8jCwUWYQFGH3lmyBi9XCBQYIvQJ-L7oF3GPUQhkp8zP-Ykb7W8D27W5qXAPkBccx3Bi4713CBv0GjAFgzcs7su9eQ-0qQVEU2z0TRlaIOx-p6a-7ppANDKynAvdKCndmQaCgYKAU0SARESFQHGX2MiYTKcw8yp_oWM4dFCq-T8bA0169'
 
 # Token validieren und decodieren
 user_info = validate_and_decode_google_token(token)

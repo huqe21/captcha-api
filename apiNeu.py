81e0a283ff3e9d1037630b7ddf7616d89b6cfe5f
Huqe21 <huqe21@icloud.com>
2024-04-15 17:06:07 +0200

    readjusted api

--- apiNeu.py
+++ apiNeu.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
-#from config import ModelConfigs
-#from imageToWordModel import ImageToWordModel
+from config import ModelConfigs
+from imageToWordModel import ImageToWordModel
 import cv2
 import os
 from apiConfig import Config
@@ -15,37 +15,37 @@ UPLOAD_FOLDER = 'uploads'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-#configs = ModelConfigs.load("Models/202403051356/configs.yaml")
+configs = ModelConfigs.load("Models/202403051356/configs.yaml")
 
-#model = ImageToWordModel(model_path=configs.model_path, char_list=configs.vocab)
+model = ImageToWordModel(model_path=configs.model_path, char_list=configs.vocab)
 
 client = MongoClient(Config.MONGO_URI)
 db = client['valid8users']
 users_collection = db ['users']
 
 
-@app.route('/captcha-solver', methods=['GET'])
+@app.route('/captcha-solver', methods=['POST'])
 def solve_captcha():
     
    
-    json =  users_collection.find()
-#     if 'file' not in request.files:
-#         return 'No file part in the request', 400
+    json =  ""
+    if 'file' not in request.files:
+        return 'No file part in the request', 400
 
-#     file = request.files['file']
-#     if file.filename == '':
-#         return 'No selected file', 400
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file', 400
 
-#     if file:
-#         filename = file.filename
-#         file.save(os.path.join(UPLOAD_FOLDER, filename))
+    if file:
+        filename = file.filename
+        file.save(os.path.join(UPLOAD_FOLDER, filename))
 
-#         image = cv2.imread(os.path.join(UPLOAD_FOLDER, filename))
-# #        prediction = model.predict(image)
-#  #       json = {'captcha': str(prediction)}
+        image = cv2.imread(os.path.join(UPLOAD_FOLDER, filename))
+        prediction = model.predict(image)
+        json = {'captcha': str(prediction)}
 
-#     if os.path.exists(os.path.join(UPLOAD_FOLDER, file.filename)):
-#         os.remove(os.path.join(UPLOAD_FOLDER, file.filename))
+    if os.path.exists(os.path.join(UPLOAD_FOLDER, file.filename)):
+        os.remove(os.path.join(UPLOAD_FOLDER, file.filename))
 
     return json, 200
 
